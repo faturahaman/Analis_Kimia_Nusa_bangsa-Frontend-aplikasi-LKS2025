c9c6599752cc603d02f6b6324c936e0fdb13a999
Muhamad Riffa Faturahman <145224983+faturahaman@users.noreply.github.com>
2025-03-18 11:58:00 +0700

    Add files via upload

---
+++ frontend/login.php
@@ -0,0 +1,42 @@
+<?php 
+// require 'db.php';
+
+if(isset($_POST['btn-login'])) {
+   $nama =  $_POST['username'];
+   $password = $_POST['password'];
+
+   $Vnama = "riffa";
+   $Vpassword = 123;
+   
+    if($nama == $Vnama && $password == $Vpassword) {
+    header("location: menu.php");
+    
+    };
+   
+}
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+<body class="center flex justify-center mt-[20vh]">
+    <form action="" method="post" class="bg-orange-200 w-[400px] p-12 rounded-lg outline-2 outline-offset-2 " >
+        <div class="text-center m-2" m-2>
+            <div class="flex justify-center item-center justify-items-center">
+            <h1 class="text-3xl font-bold">Login</h1>
+        </div>
+            <input type="text" id="txtnama" class="bg-orange-900 rounded-lg outline-1 outline-offset-2 text-orange-200 m-2 px-2 py-1 w-full" placeholder="Nama" name="username">
+            <input type="text" id="txtnama" class="bg-orange-900 rounded-lg outline-1 outline-offset-2 m-2 text-orange-200 px-2 py-1 w-full" type="password" placeholder="Password" name="password">
+            <button class="bg-orange-100 px-3 outline-1 outline-offset-2 rounded w-fit" type="submit" name="btn-login">Login</button>
+        </div>
+    </form>
+</body>
+</html>
